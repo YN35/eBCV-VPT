afd7cece8b039be0bd1ee5fb556ea0668591b8e5
YN35 <yono003500@gmail.com>
2022-12-27 19:46:54 +0900

    first commit

---
+++ lib/__init__.py
@@ -0,0 +1,6 @@
+
+from .models.vqvae import VQVAE
+from .models.gpt import VideoGPT
+from .data.data import VideoData
+from .data.download import load_vqvae, load_videogpt, load_i3d_pretrained, download
+
